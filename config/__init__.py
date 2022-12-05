9d1db3c0b46d5ab1d697ae4ad680bea143e03293
Alberto Cevallos <acvllsh@gmail.com>
2022-12-05 18:32:27 -0500

    add deposit() test

--- config/__init__.py
+++ config/__init__.py
@@ -5,3 +5,5 @@ usdc = '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48'
 name = 'NitroSwap Margin Pool: Badger-USDC'
 symbol = 'BADGER-USDC-MP'
 decimals = 18
+
+usdc_minter = '0x5b6122c109b78c6755486966148c1d70a50a47d7'
--- tests/test_vault_logic.py
+++ tests/test_vault_logic.py
@@ -1,4 +1,65 @@
 import pytest
+import brownie
+from brownie import (
+    interface
+)
+from config import (
+    usdc,
+    usdc_minter
+)
 
-def test_basic():
-    assert 1 == 1
+from rich.console import Console
+from rich.table import Table
+
+console = Console()
+
+def test_first_deposit(nitroswap, alice):
+    '''
+    scenario: first deposit 
+    1) usdc_minter mints 100 usdc to alice
+    2) alice deposits 100 USDC into the MarginPool vault and should receive 
+    100 LP tokens as a receipt
+    '''
+    (margin_pool) = nitroswap
+
+    # setup
+    table = Table(show_header=True, header_style="bold magenta")
+    table.add_column("actor", style="dim", width=12)
+    table.add_column("before")
+    table.add_column("after")
+    table.add_column("diff")
+
+    tx = interface.IERC20(usdc).mint(alice, 100e6, {'from': usdc_minter})
+    assert interface.IERC20(usdc).balanceOf(alice) == 100e6
+
+    tx = interface.IERC20(usdc).approve(margin_pool, 100e6, {'from': alice})
+    assert interface.IERC20(usdc).allowance(alice, margin_pool) == 100e6
+
+    # deposit
+    prevBaseBalAlice = interface.IERC20(usdc).balanceOf(alice)
+    prevBaseBalMarginPool = interface.IERC20(usdc).balanceOf(margin_pool)
+    prevLpBalAlice = interface.IERC20(margin_pool).balanceOf(alice)
+
+    tx = margin_pool.deposit(100e6, {'from': alice})
+
+    afterBaseBalAlice = interface.IERC20(usdc).balanceOf(alice)
+    afterBaseBalMarginPool = interface.IERC20(usdc).balanceOf(margin_pool)
+    afterLpBalAlice = interface.IERC20(margin_pool).balanceOf(alice)
+
+    assert prevBaseBalAlice > afterBaseBalAlice
+    assert prevBaseBalMarginPool < afterBaseBalMarginPool
+    assert prevLpBalAlice < afterLpBalAlice
+    assert afterLpBalAlice - prevLpBalAlice == 100e6
+    assert afterBaseBalMarginPool - prevBaseBalMarginPool == 100e6
+    assert interface.IERC20(margin_pool).totalSupply() == 100e6
+
+    # reporting
+    table.add_row('alice', str(prevBaseBalAlice), str(afterBaseBalAlice), str(prevBaseBalAlice - afterBaseBalAlice))
+    table.add_row('margin pool', str(prevBaseBalMarginPool), str(afterBaseBalMarginPool), str(prevBaseBalMarginPool -afterBaseBalMarginPool))
+    console.print('[green]CASE: deposit()[/green]')
+    console.print(table)
+
+
+
+
+    
